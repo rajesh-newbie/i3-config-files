f99291d056787908b6c92996ee661e4b99e6d36f
Rajesh Mantri <113574757+rajesh-newbie@users.noreply.github.com>
2024-10-02 17:44:27 +0530

    Update move.sh

--- move.sh
+++ move.sh
@@ -2,15 +2,19 @@
 
 # Move fonts to /usr/share/fonts
 echo "Moving fonts to respective places..."
-sudo mv JetJetBrainsMono /usr/share/fonts/
+sudo mv JetBrainsMono /usr/share/fonts/
 sudo mv Work_Sans /usr/share/fonts/
 
 # Move config files to respective places
 echo "Moving config files to respective places..."
 cd i3-config-files
 cd i3
-mv config ~/.config/i3/config
+mkdir ~/.config/i3
+mv config ~/.config/i3/
 cd ..
+rm -rf ~/.config/polybar
+rm -rf ~/.config/rofi
+rm -rf ~/.config/picom
 mv polybar ~/.config/
 mv rofi ~/.config/
 mv picom ~/.config/
